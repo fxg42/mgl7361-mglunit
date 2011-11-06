aa1ac35ada57a718a0193f6f3b0e2392ecfa7efe
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2011-11-06 17:57:29 -0500

    lighten

--- src/main/java/ca/uqam/mglunit/Assert.java
+++ src/main/java/ca/uqam/mglunit/Assert.java
@@ -1,7 +1,7 @@
 package ca.uqam.mglunit;
 
 public class Assert {
-  public static void assertEquals (Object expected, Object actual) throws AssertionError {
+  public static void assertEquals (Object expected, Object actual) {
     if (! expected.equals(actual))
       throw new AssertionError(
           String.format("expected:<%s> but was:<%s>", expected, actual));
