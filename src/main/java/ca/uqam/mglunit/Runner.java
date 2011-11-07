9f532badd2443592fd69e8d87caebc4476e360c0
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2011-11-07 07:52:06 -0500

    refactored test building in its own class

--- src/main/java/ca/uqam/mglunit/Runner.java
+++ src/main/java/ca/uqam/mglunit/Runner.java
@@ -14,41 +14,13 @@ public class Runner {
   public int run (String className) {
     try {
       specificationClass = Class.forName(className);
-      rootTestRunner = buildTestFrom(specificationClass);
+      rootTestRunner = new TestBuilder().buildTestFrom(specificationClass);
       rootTestRunner.run(results);
       return 0;
     } catch (Exception ex) {
       logger.log(Level.SEVERE, "Caught expected", ex);
       return 1;
     }
-  }
-
-  private TestRunner buildTestFrom (Class specificationClass) throws Exception {
-    for (Field each : specificationClass.getFields())
-      if (each.isAnnotationPresent(TestCases.class))
-        return buildTestSuiteFrom(specificationClass, each);
-    return buildTestCaseFrom(specificationClass);
-  }
-
-  @SuppressWarnings("unchecked")
-  private TestRunner buildTestSuiteFrom (Class specificationClass, Field testCaseClassesField) throws Exception {
-    Object specification = specificationClass.newInstance();
-    TestSuiteRunner suite = new TestSuiteRunner(specification);
-    Class[] testCaseClasses = (Class[]) testCaseClassesField.get(specification);
-    for (Class each : testCaseClasses)
-      suite.addTest(buildTestFrom(each));
-    return suite;
-  }
-
-  private TestRunner buildTestCaseFrom (Class specificationClass) throws Exception {
-    Object specification = specificationClass.newInstance();
-    TestCaseRunner testcase = new TestCaseRunner(specification);
-    for (Method each : specificationClass.getMethods()) {
-      if (each.isAnnotationPresent(Test.class)) testcase.addTestMethod(each);
-      if (each.isAnnotationPresent(Setup.class)) testcase.setSetupMethod(each);
-      if (each.isAnnotationPresent(Teardown.class)) testcase.setTeardownMethod(each);
-    }
-    return testcase;
   }
 
   public void setTestResultLogger (TestResultLogger testResultLogger) {
---
+++ src/main/java/ca/uqam/mglunit/TestBuilder.java
@@ -0,0 +1,34 @@
+package ca.uqam.mglunit;
+
+import java.lang.reflect.*;
+
+class TestBuilder {
+
+  public TestRunner buildTestFrom (Class specificationClass) throws Exception {
+    for (Field each : specificationClass.getFields())
+      if (each.isAnnotationPresent(TestCases.class))
+        return buildTestSuiteFrom(specificationClass, each);
+    return buildTestCaseFrom(specificationClass);
+  }
+
+  @SuppressWarnings("unchecked")
+  public TestRunner buildTestSuiteFrom (Class specificationClass, Field testCaseClassesField) throws Exception {
+    Object specification = specificationClass.newInstance();
+    TestSuiteRunner suite = new TestSuiteRunner(specification);
+    Class[] testCaseClasses = (Class[]) testCaseClassesField.get(specification);
+    for (Class each : testCaseClasses)
+      suite.addTest(buildTestFrom(each));
+    return suite;
+  }
+
+  public TestRunner buildTestCaseFrom (Class specificationClass) throws Exception {
+    Object specification = specificationClass.newInstance();
+    TestCaseRunner testcase = new TestCaseRunner(specification);
+    for (Method each : specificationClass.getMethods()) {
+      if (each.isAnnotationPresent(Test.class)) testcase.addTestMethod(each);
+      if (each.isAnnotationPresent(Setup.class)) testcase.setSetupMethod(each);
+      if (each.isAnnotationPresent(Teardown.class)) testcase.setTeardownMethod(each);
+    }
+    return testcase;
+  }
+}
